6de7788d7761249eb0fc0bbabc02793f5631fb4c
raysulkobir <raysulkobir97@gmail.com>
2022-01-23 00:29:35 +0600

    first

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+"use strict";
+// function sumFun(a: any, b:any){
+//     return a * b;
+// }
+// console.log("sum", sumFun(10, 50))
+// part-2
+let data = [];
+data.push(10);
+data.push("hello");
+
+console.log("data", data)
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+// function sumFun(a: any, b:any){
+//     return a * b;
+// }
+
+// console.log("sum", sumFun(10, 50))
+
+
+
+// part-2
+let data = [];
+data.push(10);
+data.push("hello");
+

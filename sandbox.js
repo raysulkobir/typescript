61c602e8df79b824d772c5c5d9a4cfccb5ef9998
raysulkobir <raysulkobir97@gmail.com>
2022-01-25 02:07:32 +0600

    typescript class done

--- sandbox.js
+++ sandbox.js
@@ -4,8 +4,80 @@
 // }
 // console.log("sum", sumFun(10, 50))
 // part-2
+//  function greeter(person) {  
+//     return "Hello, " + person;  
+// }  
+// let user = 'JavaTpoint';  
+// console.log(greeter(user));  
+// part-3
+// let dataName = 5;
+// dataName = 10;
+// dataName = 6;
+// console.log(dataName);
+// let data = ["rubel", "kobir"];
+// data.push("jamal");
+// console.log(data);
+// let person = {
+//     name: "Md.Raysul kobir",
+//     age: 10,
+//     county: "d",
+//
+// }
+// person.county = "Bangaldesh"
+// console.log(person)
+// part-4
+// let a: number;
+//     a = 10;
+// let data:(string | number)[] = [];
+// data.push("10")
+// data.push(100);
+// let c: object | string;
+// c = {
+//     name: "kamal",
+//     age: 10
+// }
+// console.log('data', c)
+// part-6
+// let myFun:Function;
+// myFun = (a:number, b:number, c:number = 0)=>{
+//     return a + b + c;
+// }
+//
+// console.log(myFun(10, 30))
+// part-7
+// let  user =  {
+//     age: 10,
+//     name: "Md.Raysul"
+// }
+//
+// type userType = {age: number, name: string}
+// const sayHello = (user:userType) => {
+//     console.log(`Hello ${user.age} Name: ${user.name}`)
+// }
+// sayHello(user)
+// part-8
+// let cal: (a: number, b: number, c: string) => number;
+// cal = (a: number, b: number, c: string) =>{
+//     return a+b;
+// }
+//
+// console.log(cal(10, 4, "add"))
+// part-9
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        return (`${this.name} age: ${this.age} country ${this.country}`);
+    }
+}
+let sajid = new Player("Md.Raysul kobir", 20, "bangllades");
+let sanit = new Player("Md.Sanid", 15, "bangllades");
 let data = [];
-data.push(10);
-data.push("hello");
-
-console.log("data", data)
+data.push(sajid);
+data.push(sanit);
+// data.push("name")
+console.log(data);
+// console.log("play", sanit.play())
